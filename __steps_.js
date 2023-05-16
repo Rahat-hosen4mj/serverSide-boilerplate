e49efb74bf4bb68fe11ba318170027d9642c7a9c
Your Name <you@example.com>
2023-05-16 21:47:21 +0600

    initial project set up is ready

---
+++ __steps_.js
@@ -0,0 +1,51 @@
+/**
+ * -----------------------------------------------
+ *  WHAT YOU WILL DO AFTER DOWNLOAD THIS PROJECT
+ * -----------------------------------------------
+ * 1. At first npm install
+ * 2. create mongoDb Database collection and change with your project
+ * 3. change app.listen port and single app.get route text mgs
+ * 4. 
+ * 
+ * 
+ * 
+ * ---------------------------
+ *  for create a new server site
+ * -----------------------------
+ * 1. npm init --y
+ * 2 . npm i mongodb cors dotenv express
+ * 
+ * =========================================
+ *  FOR Vercel deploy (p7biF5VCX3Y6hpOP)
+ * ======================================
+ * 1. open the project file then type == vercel
+ * 2 set up and deploy - y
+ * 3. which scope do you want to depliy == jeta diey vercel account khulco
+ * 4. link to existing project - N
+ * 5. what's your project name [give your porject name]
+ * 6. In which directory is your code located [./]
+ * 
+ * Production er jonno [vercel --prod] (env file eh db.pass & db user add korte hobe)
+ * 1. goto vercel website and open your project
+ * 2. goto setting option && look for (environment variable)
+ * 3. add all the credential then save
+ * 
+ * =========================================
+ *  FOR firebase deploy 
+ * ======================================
+ * 1. goto firebase website and looking for hosting
+ * 2. firebase init
+ * 3. are you ready to proceed - y
+ * 4 hosting (press enter key) configure file for firebase hosting
+ * 5. what do you want to as your public directory - dist
+ * 6. configure as single file - y
+ * 7. automethic build with github - n
+ * 9. overwrite korte chaw - y
+ * 10. npm run build
+ * 11. firebase deploy
+ * 
+ * 
+ * 
+ */
+
+
--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mrrahathosen
-// p7biF5VCX3Y6hpOP
-const uri = "mongodb+srv://mrrahathosen:p7biF5VCX3Y6hpOP@cluster0.zdki8if.mongodb.net/?retryWrites=true&w=majority";
+// mrrahathosen 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zdki8if.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +26,18 @@ async function run() {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
+
+      const serviceCollection = client.db('car_doctor').collection('services');
+    //   const bookingCollection = client.db('carDoctor').collection('bookings');
+
+     // services routes
+     app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,7 +52,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('server side  is running now....')
+    res.send('server side  is running now....!!!')
 })
 
 app.listen(port, () => {
